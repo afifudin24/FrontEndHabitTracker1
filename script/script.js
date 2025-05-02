01dffb5da192eddfde4d295552f56271dd71f28f
afifudin24 <afifrider507@gmail.com>
2025-05-02 22:09:53 +0700

    Revision Dashboard Page

--- script/script.js
+++ script/script.js
@@ -1,12 +1,16 @@
 const h1Text = [
-    "Your New Habit Starts Today", "Track. Improve. Repeat.", "Master Your Daily Routine" ,"Stay Consistent, Stay Empowered", "Small Steps, Big Changes"
-]
+  'Your New Habit Starts Today',
+  'Track. Improve. Repeat.',
+  'Master Your Daily Routine',
+  'Stay Consistent, Stay Empowered',
+  'Small Steps, Big Changes',
+];
 
-$(document).ready(function(){
-    setInterval(function(){
-        let randomNum = Math.floor(Math.random() * h1Text.length);
-        $(".h1Text").fadeOut(400, function() {
-            $(this).text(h1Text[randomNum]).fadeIn(400);
-        });
-    }, 3000); // Disarankan 3 detik agar lebih enak dibaca
+$(document).ready(function () {
+  setInterval(function () {
+    let randomNum = Math.floor(Math.random() * h1Text.length);
+    $('.h1Text').fadeOut(400, function () {
+      $(this).text(h1Text[randomNum]).fadeIn(400);
+    });
+  }, 3000); // Disarankan 3 detik agar lebih enak dibaca
 });
